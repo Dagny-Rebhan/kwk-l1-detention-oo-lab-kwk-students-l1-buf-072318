78d7b81331762d7d6bb41bd954721c6c276c0a1e
Dagny-Rebhan <drebhan@buffaloseminary.org>
2018-07-30 12:40:09 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,1 +1,11 @@
 #Code your detention class here
+
+class Detention 
+  attr_reader :test_activity
+  attr_accessor :location, :time, :grumpy_teacher
+    def initialize(test_activity)
+     @test_activity = collective_punishment
+    end 
+end 
+
+
